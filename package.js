797a50dd07816b8315e90b3e9de9f13877f6a6b8
Luke Nimtz <luke.nimtz@gmail.com>
2018-09-26 16:30:14 -0700

    not imposing timeout on tests

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "protometa:browser-tests",
   summary: "A helper package for Meteor test driver packages. Runs client tests in a headless browser.",
   git: "https://github.com/protometa/meteor-browser-tests.git",
-  version: '0.1.4',
+  version: '0.1.5',
   testOnly: true,
 });
 
